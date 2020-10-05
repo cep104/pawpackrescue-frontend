a0ab3e87457a7846541577825cedf64b704bf4d4
cep104 <candicelizabethpeters@gmail.com>
2020-10-05 18:54:57 -0500

    caretaker reducer defined

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-
+import caretakerReducer from './reducers/caretakerReducer'
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(caretakerReducer, composeEnhancers(applyMiddleware(thunk)))
 //  store is where your storing your data globally 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/caretakerReducer.js
@@ -0,0 +1,3 @@
+export default function caretakerReducer(state = {caretaker: []}, action){
+    return state
+}
