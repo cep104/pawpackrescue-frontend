da64d66bd00f7d43442089877c401e4c63402d11
cep104 <candicelizabethpeters@gmail.com>
2020-10-05 17:30:32 -0500

    redux packages added to index

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/caretakers')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/caretakers/1/dogs')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data[0]))
+  // }
   render(){
   return (
     <div className="App">
