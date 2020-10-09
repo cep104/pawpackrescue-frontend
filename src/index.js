9c4350341dfa0e4c59f90aaccb121e0bd959637b
cep104 <candicelizabethpeters@gmail.com>
2020-10-09 13:17:13 -0500

    routes added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import caretakerReducer from './reducers/caretakerReducer'
 import App from './App';
 
@@ -12,7 +13,9 @@ let store = createStore(caretakerReducer, composeEnhancers(applyMiddleware(thunk
 //  store is where your storing your data globally 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <Router>
+      <App />
+      </Router>
   </Provider>
   ,
   document.getElementById('root')
