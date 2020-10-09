0066db8db43e3dc121ec3c5798f48130e06618c4
cep104 <candicelizabethpeters@gmail.com>
2020-10-08 21:40:58 -0500

    debugging and handleSubmit for caretaker created

--- src/components/Caretakerinput.js
+++ src/components/Caretakerinput.js
@@ -6,17 +6,30 @@ class Caretakerinput extends React.Component {
         location:'',
         setting:'' 
     }
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+       this.setState({
+           [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        
+    }
  
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Name: </label>
-                    <input type='text' placeholder = 'Name' value={this.state.name}/><br/>
+                    <input type='text' placeholder = 'Name' name='name' value={this.state.name} onChange={this.handleChange}/><br/>
                     <label>Location: </label>
-                    <input type='text' placeholder = 'Location'value={this.state.location}/><br/>
+                    <input type='text' placeholder = 'Location' name='location' value={this.state.location} onChange={this.handleChange}/><br/>
                     <label >Type: </label>
-                    <input type='text' placeholder = 'Type' value={this.state.setting}/><br/>
+                    <input type='text' placeholder = 'Type' name='setting' value={this.state.setting} onChange={this.handleChange}/><br/>
+                    <input type='submit' />
                 </form>
             </div>
      )
