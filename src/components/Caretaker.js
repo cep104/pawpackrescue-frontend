3a1e5d895ddfd72c0c32611d743f5423455d1043
cep104 <candicelizabethpeters@gmail.com>
2020-10-09 17:27:26 -0500

    added dog container

--- src/components/Caretaker.js
+++ src/components/Caretaker.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
+import DogContainer from '../containers/DogContainer'
 const Caretaker = (props) => {
     
     let caretaker = props.caretakers[props.match.params.id - 1]
     return (
         <div>
             {/* { caretaker ? null : <Redirect to='/caretakers'/>} */}
-           {caretaker ? caretaker.name : null }
+           <h1>{caretaker ? caretaker.name : null }</h1>
            {caretaker ? caretaker.location : null }
            {caretaker ? caretaker.setting : null }
-           
+           <DogContainer />
         </div>
     )
 
---
+++ src/containers/DogContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class DogContainer extends React.Component {
+   render(){
+       return(
+           <div>
+               DogContainer
+           </div>
+       )
+   } 
+}
+
+export default DogContainer
+
