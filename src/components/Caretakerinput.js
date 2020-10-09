f7bcf17b964e5abefd6b97de2bf8ed17be215769
cep104 <candicelizabethpeters@gmail.com>
2020-10-08 20:33:02 -0500

    form added to caretaker input

--- src/components/Caretakerinput.js
+++ src/components/Caretakerinput.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 class Caretakerinput extends React.Component {
- render(){
-     return(
-         <div>
-             Caretaker Input
-         </div>
+    state = {
+        name:'',
+        location:'',
+        setting:'' 
+    }
+ 
+    render(){
+        return(
+            <div>
+                <form>
+                    <label>Name: </label>
+                    <input type='text' placeholder = 'Name' value={this.state.name}/><br/>
+                    <label>Location: </label>
+                    <input type='text' placeholder = 'Location'value={this.state.location}/><br/>
+                    <label >Type: </label>
+                    <input type='text' placeholder = 'Type' value={this.state.setting}/><br/>
+                </form>
+            </div>
      )
  }
 }
