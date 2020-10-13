cdd1fe7e2855bbfc577d729c58be26d9ebd1e686
cep104 <candicelizabethpeters@gmail.com>
2020-10-13 10:53:20 -0500

    dog info

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -3,7 +3,7 @@ import DeleteDog from './DeleteDog'
 
 import {Link} from 'react-router-dom'
 const Dogs = (props) => {
-
+   
     return (
         <div id='dogcards'>
             {props.dogs && props.dogs.map(dog => 
@@ -13,7 +13,11 @@ const Dogs = (props) => {
                        <Link to={`/dogs/${dog.id}`}>{dog.name}</Link><br/>
                        <p>Age: {dog.age}</p>
                        <p>Gender: {dog.gender}</p>
+                       <p>Breed: {dog.breed}</p>
+                       <p>Size: {dog.size}</p>
+                       <p>Potty Trained: {dog.house_trained}</p>
                        <p>Gets along with: {dog.good_with}</p>
+                       <p>Medication: {dog.medication}</p>
                        <p>{dog.bio}</p>
                        <DeleteDog dog={dog}/>
                     </div>
