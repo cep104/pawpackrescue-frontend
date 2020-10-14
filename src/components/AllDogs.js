d139d4fcf37316e88c9c90e41850b5f27729c21b
cep104 <candicelizabethpeters@gmail.com>
2020-10-14 14:53:34 -0500

    css changes added color to hover for dogs

--- src/components/AllDogs.js
+++ src/components/AllDogs.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 const Dogs = props => {
     if (props.dogs.length === 0) return null;
     const dogs = props.dogs.map((dog, index) => {
-        return <div key={index} className="dogList">
-            <Link
+        return <div key={index} className="card">
+            <img src={dog.img_src}/>
+         <div className='container'>   <Link
                 to={`/dogs/${dog.id}`}
-            ><h2>{dog.name}</h2></Link>
-        </div>
+            ><h2>{dog.name}</h2><p>Age: {dog.age}</p></Link>
+        </div></div>
     })
     return (
-        <div>{dogs}</div>
+        <div >{dogs}</div>
     );
 };
 export default Dogs;
