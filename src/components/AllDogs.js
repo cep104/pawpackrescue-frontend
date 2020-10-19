7138e3e12e916230b177b80bc7cf918f31ca8a48
cep104 <candicelizabethpeters@gmail.com>
2020-10-18 22:55:13 -0500

    css changes

--- src/components/AllDogs.js
+++ src/components/AllDogs.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom'
 const Dogs = props => {
     if (props.dogs.length === 0) return null;
     const dogs = props.dogs.map((dog, index) => {
-        return <div key={index} className="card">
-            <img src={dog.img_src}/>
-         <div className='container'>   <Link
-                to={`/dogs/${dog.id}`}
-            ><h2>{dog.name}</h2><p>Age: {dog.age}</p></Link>
-        </div></div>
+        return <Link to={`/dogs/${dog.id}`}>
+                    <div key={index} className="card">
+                        <img src={dog.img_src}/>
+                        <div className='container'>   
+                            <h2>{dog.name}</h2>
+                            <p>Age: {dog.age}</p>
+                        </div>
+                    </div>
+                 </Link>
     })
     return (
         <div >{dogs}</div>
--- src/components/Caretakerlist.js
+++ src/components/Caretakerlist.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom'
-import Caretaker from './Caretaker'
+import { Link } from 'react-router-dom'
 
 const Caretakerlist = ({caretakers}) => {
     
