598377cb77d98a112d37a66d15c4f3cc46f5b37f
cep104 <candicelizabethpeters@gmail.com>
2020-10-19 10:35:18 -0500

    css changed display of dog

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -2,8 +2,10 @@ import React from 'react';
 const Dog = ({ match, dogs }) => {
 
     if (dogs.length === 0) return null;
-
+    
     let dog = dogs.find(d => d.id === parseInt(match.params.id))
+    if (dog.house_trained === true) { dog.house_trained = 'Yes'} 
+    else { dog.house_trained = 'No'}
     return (
         <>
         <div>
@@ -17,6 +19,7 @@ const Dog = ({ match, dogs }) => {
                 <p>Size: {dog ? dog.size : null }</p>
                 <p>Gets Along with: {dog ? dog.good_with: null }</p>
                 <p>Medication Needs: {dog ? dog.medication : null }</p>
+                <p>House Trained: { dog ? dog.house_trained.toString() : null }</p>
                 <p>Bio: {dog ? dog.bio : null }</p>
             </div>
         </div>
